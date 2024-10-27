eaf8a6f4ae598b469ab08a477af485734b25c663
Vaughan Whitteron <github@firetooth.net>
2024-10-27 20:17:02 +0900

    chore: cleanup

--- internal/audioeffects/audioeffects.go
+++ internal/audioeffects/audioeffects.go
@@ -37,52 +37,13 @@ func generateGearChangeSample(sampleRateHz int) []float64 {
 	waveOffset := float64(pulseWidth)
 
 	audioSample := make([]float64, sampleCount)
-	// audioSampleInt := make([]int, sampleCount)
 
 	for i := range audioSample {
 		angle := waveSamplePeriod * (float64(i) - waveOffset)
 		audioSample[i] = (pulseAmplitude * math.Sin(angle)) / 2
 
-		// audioSampleInt[i] = int(audioSample[i] * 32767)
-
 		pulseAmplitude = pulseAmplitude * (1 - decayRate)
 	}
-
-	// fmt.Printf("\n\nSample: %+v\n\n", audioSample)
-	// fmt.Printf("\n\nSample: %+v\n\n", audioSampleInt)
-
-	// out, err := os.Create("gearchange.wav")
-	// if err != nil {
-	// 	panic(fmt.Sprintf("couldn't create output file - %v", err))
-	// }
-
-	// bitDepth := 16
-	// channels := 1
-	// format := 1
-
-	// buf := &goaudio.IntBuffer{
-	// 	Format: &goaudio.Format{
-	// 		NumChannels: channels,
-	// 		SampleRate:  sampleRateHz,
-	// 	},
-	// 	SourceBitDepth: bitDepth,
-	// 	Data:           audioSampleInt,
-	// }
-
-	// e := wav.NewEncoder(out,
-	// 	sampleRateHz,
-	// 	bitDepth,
-	// 	channels,
-	// 	format)
-	// if err = e.Write(buf); err != nil {
-	// 	panic(err)
-	// }
-	// // close the encoder to make sure the headers are properly
-	// // set and the data is flushed.
-	// if err = e.Close(); err != nil {
-	// 	panic(err)
-	// }
-	// out.Close()
 
 	return audioSample
 }
