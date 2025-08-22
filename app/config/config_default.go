9b5e55753ad6a4577db3c4b4ce2cecb5dd3e7e6d
Vaughan Whitteron <github@firetooth.net>
2025-08-22 13:04:11 +0900

    chore: config cleanup and doc

--- app/config/config_default.go
+++ app/config/config_default.go
@@ -1,3 +1,5 @@
+//lint:file-ignore SA4026
+
 package config
 
 import appHaptics "github.com/vwhitteron/simtezilo-dev/app/haptics"
@@ -24,10 +26,8 @@ var defaultConfig = viperConfig{
 		PulseMinFrequencyHz:          16,
 		_pulseWidthMax:               0.5,
 		_pulseWidthMin:               0.1,
-		EngineFrequencyMin:           15,
-		EngineFrequencyMax:           45,
 		EngineProfiles: map[string]appHaptics.EngineProfile{
-			"s1":           {PrimaryBalance: 0.15, SecondaryBalance: 0.250, Gain: -0.0, PulseScale: 1.00},
+			"s1":           {PrimaryBalance: 0.15, SecondaryBalance: 0.250, Gain: +0.0, PulseScale: 1.00},
 			"i2":           {PrimaryBalance: 0.65, SecondaryBalance: 0.850, Gain: -2.0, PulseScale: 1.00},
 			"i3":           {PrimaryBalance: 0.95, SecondaryBalance: 0.850, Gain: -2.0, PulseScale: 1.00},
 			"i4":           {PrimaryBalance: 0.60, SecondaryBalance: 0.800, Gain: -3.5, PulseScale: 0.50},
@@ -35,14 +35,14 @@ var defaultConfig = viperConfig{
 			"i6":           {PrimaryBalance: 0.90, SecondaryBalance: 0.950, Gain: -2.5, PulseScale: 0.50},
 			"i8":           {PrimaryBalance: 0.95, SecondaryBalance: 0.980, Gain: -1.5, PulseScale: 0.50},
 			"v4":           {PrimaryBalance: 0.65, SecondaryBalance: 0.850, Gain: -2.5, PulseScale: 1.00},
-			"v6":           {PrimaryBalance: 0.85, SecondaryBalance: 0.960, Gain: -0.0, PulseScale: 0.50},
-			"v6.b15.c120":  {PrimaryBalance: 0.88, SecondaryBalance: 0.940, Gain: -0.0, PulseScale: 0.50},
-			"v6.b75.c120":  {PrimaryBalance: 0.83, SecondaryBalance: 0.940, Gain: -0.0, PulseScale: 0.50},
-			"v6.b80.c120":  {PrimaryBalance: 0.82, SecondaryBalance: 0.930, Gain: -0.0, PulseScale: 0.50},
-			"v6.b90.c120":  {PrimaryBalance: 0.80, SecondaryBalance: 0.920, Gain: -0.0, PulseScale: 0.50},
-			"v6.b120.c120": {PrimaryBalance: 0.75, SecondaryBalance: 0.880, Gain: -0.0, PulseScale: 0.50},
+			"v6":           {PrimaryBalance: 0.85, SecondaryBalance: 0.960, Gain: +0.0, PulseScale: 0.50},
+			"v6.b15.c120":  {PrimaryBalance: 0.88, SecondaryBalance: 0.940, Gain: +0.0, PulseScale: 0.50},
+			"v6.b75.c120":  {PrimaryBalance: 0.83, SecondaryBalance: 0.940, Gain: +0.0, PulseScale: 0.50},
+			"v6.b80.c120":  {PrimaryBalance: 0.82, SecondaryBalance: 0.930, Gain: +0.0, PulseScale: 0.50},
+			"v6.b90.c120":  {PrimaryBalance: 0.80, SecondaryBalance: 0.920, Gain: +0.0, PulseScale: 0.50},
+			"v6.b120.c120": {PrimaryBalance: 0.75, SecondaryBalance: 0.880, Gain: +0.0, PulseScale: 0.50},
 			"v8":           {PrimaryBalance: 0.95, SecondaryBalance: 0.980, Gain: -3.0, PulseScale: 0.25},
-			"v8.b90.c180":  {PrimaryBalance: 0.85, SecondaryBalance: 0.920, Gain: -0.0, PulseScale: 0.50},
+			"v8.b90.c180":  {PrimaryBalance: 0.85, SecondaryBalance: 0.920, Gain: +0.0, PulseScale: 0.50},
 			"v10":          {PrimaryBalance: 0.88, SecondaryBalance: 0.965, Gain: -2.0, PulseScale: 0.25},
 			"v10.b90.v72":  {PrimaryBalance: 0.86, SecondaryBalance: 0.940, Gain: -1.5, PulseScale: 0.25},
 			"v12":          {PrimaryBalance: 0.99, SecondaryBalance: 0.995, Gain: -2.0, PulseScale: 0.25},
@@ -51,12 +51,12 @@ var defaultConfig = viperConfig{
 			"v12.b100.c60": {PrimaryBalance: 0.95, SecondaryBalance: 0.980, Gain: -1.5, PulseScale: 0.25},
 			"v12.b144.c60": {PrimaryBalance: 0.93, SecondaryBalance: 0.975, Gain: -1.5, PulseScale: 0.25},
 			"w16":          {PrimaryBalance: 0.99, SecondaryBalance: 0.995, Gain: -1.5, PulseScale: 0.25},
-			"h2":           {PrimaryBalance: 0.70, SecondaryBalance: 0.950, Gain: -0.0, PulseScale: 1.00},
-			"h4":           {PrimaryBalance: 0.80, SecondaryBalance: 0.950, Gain: -0.0, PulseScale: 1.00},
+			"h2":           {PrimaryBalance: 0.70, SecondaryBalance: 0.950, Gain: +0.0, PulseScale: 1.00},
+			"h4":           {PrimaryBalance: 0.80, SecondaryBalance: 0.950, Gain: +0.0, PulseScale: 1.00},
 			"h6":           {PrimaryBalance: 0.92, SecondaryBalance: 0.975, Gain: -1.5, PulseScale: 0.50},
-			"h12":          {PrimaryBalance: 0.98, SecondaryBalance: 0.990, Gain: -0.0, PulseScale: 0.25},
-			"K2":           {PrimaryBalance: 0.85, SecondaryBalance: 0.960, Gain: -0.0, PulseScale: 0.33},
-			"K4":           {PrimaryBalance: 0.75, SecondaryBalance: 0.880, Gain: -0.0, PulseScale: 0.16},
+			"h12":          {PrimaryBalance: 0.98, SecondaryBalance: 0.990, Gain: +0.0, PulseScale: 0.25},
+			"K2":           {PrimaryBalance: 0.85, SecondaryBalance: 0.960, Gain: +0.0, PulseScale: 0.33},
+			"K4":           {PrimaryBalance: 0.75, SecondaryBalance: 0.880, Gain: +0.0, PulseScale: 0.16},
 		},
 	},
 	Synthesizer: &Synthesizer{
