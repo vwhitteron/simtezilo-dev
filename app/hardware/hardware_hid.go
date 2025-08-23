d9df4163366c0ee27d894c9f05255fa02e08491c
Vaughan Whitteron <github@firetooth.net>
2025-08-23 14:31:48 +0900

    feat: add auto-repeat when button held down

--- app/hardware/hardware_hid.go
+++ app/hardware/hardware_hid.go
@@ -12,7 +12,13 @@ import (
 const (
 	debouncedLow  = 0x00
 	debouncedHigh = 0xFF
-	sampleRate    = 5 * time.Millisecond
+
+	gpioReadSampleRate = 5 * time.Millisecond // Rate at which GPIO pin is sampled for debouncing
+
+	autoRepeatInitialDelay = 500 * time.Millisecond // Initial delay before button auto-repeat starts
+	autoRepeatMaxTime      = 2 * time.Second        // Time to reach max auto-repeat rate
+	autoRepeatInitialRate  = 200 * time.Millisecond // Initial auto-repeat rate
+	autoRepeatMaxRate      = 50 * time.Millisecond  // Fastest auto-repeat rate
 )
 
 func OnGPIOButtonPressed(n int, fn func()) {
@@ -33,7 +39,7 @@ func OnGPIOButtonPressed(n int, fn func()) {
 			p.WaitForEdge(-1)
 
 			// Start debounce sampling when edge is detected
-			ticker := time.NewTicker(sampleRate)
+			ticker := time.NewTicker(gpioReadSampleRate)
 
 		debouncer:
 			for {
@@ -45,9 +51,39 @@ func OnGPIOButtonPressed(n int, fn func()) {
 						if stableLevel != lastStableLevel {
 							lastStableLevel = stableLevel
 
-							// Trigger callback only on stable transition to LOW (button pressed with pull-up)
+							// Trigger callback on stable transition to LOW (button pressed with pull-up)
 							if stableLevel == gpio.Low {
 								fn()
+
+								// Auto-repeat when the button is held down
+								go func() {
+									// Initial delay before repeat begins
+									time.Sleep(autoRepeatInitialDelay)
+
+									repeatStartTime := time.Now()
+
+									for {
+										// Exit automatic repeat if button has been released
+										if p.Read() == gpio.High {
+											return
+										}
+
+										fn()
+
+										elapsed := time.Since(repeatStartTime)
+
+										// Linear acceleration of repeat rate
+										acceleration := min(
+											1.0,
+											float64(elapsed)/float64(autoRepeatMaxTime),
+										)
+
+										// Interpolate between initial and acclerated repeat rates
+										repeatRate := float64(autoRepeatInitialRate) - (float64(autoRepeatInitialRate-autoRepeatMaxRate) * acceleration)
+
+										time.Sleep(time.Duration(repeatRate))
+									}
+								}()
 							}
 						}
 						break debouncer
