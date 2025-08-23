bd5a9d44a01e8b283cee92d269ce90ce034217c6
Vaughan Whitteron <github@firetooth.net>
2025-08-23 15:26:26 +0900

    chore: add todo

--- app/hardware/spotpear/spotpear_hid.go
+++ app/hardware/spotpear/spotpear_hid.go
@@ -5,6 +5,7 @@ import (
 	"github.com/vwhitteron/simtezilo-dev/app/ui"
 )
 
+// TODO: implement button rotation logic
 func SetupHID(hidEvent chan ui.HIDInputEvent) {
 	OnButtonUpPressed(func() {
 		hidEvent <- ui.HIDInputUp
