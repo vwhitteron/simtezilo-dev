e0fae2c2b32ee5e837633e44849b9b284b726187
Vaughan Whitteron <github@firetooth.net>
2025-09-26 15:50:39 +0900

    refactor: move updateCircuit to separate file and remove unnecessary fuel range reset

---
+++ app/app_circuit.go
@@ -0,0 +1,18 @@
+package app
+
+import (
+	"github.com/zetetos/gt-telemetry/pkg/models"
+)
+
+// updateCircuit checks for circuit changes and resets odometer and fuel range if needed
+func (a *App) updateCircuit() {
+	lap := a.state.current.lapNumber
+	odometer := a.odometer.Read()
+	coordinates := a.gtClient.Telemetry.PositionalMapCoordinates()
+
+	if didUpdate := a.circuit.UpdateCircuit(odometer, lap, coordinates, models.CoordinateTypeCircuit); didUpdate {
+		a.odometer.Reset()
+		a.fuelRange.Reset()
+		a.state.last.lastLapTime = 0
+	}
+}
--- app/app_fuel_range.go
+++ app/app_fuel_range.go
@@ -6,28 +6,12 @@ func (a *App) updateFuelRange() {
 		return
 	}
 
-	if a.timeOfDayHasReset() {
-		a.odometer.Reset()
-		a.fuelRange.Reset()
-		a.circuit.ResetLapProgress()
-	}
-
-	lap := a.state.current.lapNumber
+	// lap := a.state.current.lapNumber
 	coordinates := a.gtClient.Telemetry.PositionalMapCoordinates()
 	odometerReading := a.odometer.Add(coordinates)
 
 	fuelLevel := a.gtClient.Telemetry.FuelLevelPercent()
 	a.fuelRange.Update(odometerReading, fuelLevel)
 
-	a.circuit.UpdateDistanceTravelled(odometerReading, lap, circuit.CoordinateTypeGeneral)
-}
-
-func (a *App) updateCircuit() {
-	coordinates := a.gtClient.Telemetry.PositionalMapCoordinates()
-
-	if didUpdate := a.circuit.UpdateCircuit(coordinates, circuit.CoordinateTypeGeneral); didUpdate {
-		a.odometer.Reset()
-		a.fuelRange.Reset()
-		a.state.last.lastLapTime = 0
-	}
+	// a.circuit.UpdateDistanceTravelled(odometerReading, lap, models.CoordinateTypeCircuit)
 }
